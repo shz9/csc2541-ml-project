c5f9828d55f334155a9ba17f66e36e8176b3ba38
Shadi <shadizabad1@gmail.com>
2017-11-14 22:16:22 -0500

    Adding yeast cell expression dataset from Spellman et al. 1998.

--- gene_expression_experiments/data_loader.py
+++ gene_expression_experiments/data_loader.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import scale
 
 
-def read_tp63_dataset():
+def read_tp63_dataset(drop_nan=False):
     """
 
     TRP63 Dataset from Della Gatta et al. 2008.
@@ -24,9 +23,45 @@ def read_tp63_dataset():
                              index=tp63_data.index,
                              columns=tp63_data.columns)
 
-    # Filter out profiles where more than half of
-    # the measurements are NaN:
-
-    tp63_data = tp63_data.loc[tp63_data.count(1) > tp63_data.shape[1] / 2, ]
+    if drop_nan:
+        tp63_data = tp63_data.dropna()
+    else:
+        # Filter out profiles where more than half of
+        # the measurements are NaN:
+        tp63_data = tp63_data.loc[tp63_data.count(1) > tp63_data.shape[1] / 2, ]
 
     return tp63_data
+
+
+def read_yeast_cell_cycle_dataset(time_course="alpha", drop_nan=False):
+    """
+
+    Possible values for the time_course parameter:
+    - alpha
+    - cdc15
+    - cdc28
+    - elu
+
+    The dataset was retrieved from
+    http://genome-www.stanford.edu/cellcycle/data/rawdata/
+    Retrieval date: Nov. 14, 2017
+
+    """
+
+    yeast_data = pd.read_csv("yeast_data.txt",
+                             header=0,
+                             index_col=0,
+                             sep="\t")
+
+    # Filter out other time courses / delimiter columns:
+    yeast_data = yeast_data.loc[:, [cl for cl in yeast_data.columns
+                                    if time_course in cl and time_course != cl]]
+
+    if drop_nan:
+        yeast_data = yeast_data.dropna()
+    else:
+        # Filter out profiles where more than half of
+        # the measurements are NaN:
+        yeast_data = yeast_data.loc[yeast_data.count(1) > yeast_data.shape[1] / 2, ]
+
+    return yeast_data
