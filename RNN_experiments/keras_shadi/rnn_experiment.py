d25503d9049970b8d4e4e627db2480ae5a8af930
Shadi <shadizabad1@gmail.com>
2017-11-21 17:11:07 -0500

    Parallelizing predictions

--- RNN_experiments/keras_shadi/rnn_experiment.py
+++ RNN_experiments/keras_shadi/rnn_experiment.py
@@ -6,8 +6,10 @@ from keras.layers import LSTM
 from keras.models import load_model, save_model
 from matplotlib import pyplot as plt
 import numpy as np
+from scipy import stats
 import glob
 import os
+import pickle
 from joblib import Parallel, delayed
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
 from RNN_experiments.bootstrap.other_bootstrap import stationary_boostrap_method, \
@@ -55,12 +57,13 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
     model = Sequential()
     model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    training_acc = None
     for i in range(nb_epoch):
-        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+        train_res = model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+        training_acc = train_res.history['acc']
         model.reset_states()
-        print("Training Epoch", i, "done...")
-    return model
+    return model, training_acc
 
 
 # make a one-step forecast
@@ -76,13 +79,16 @@ def train_model(ith_dataset, idx):
     # transform data to be supervised learning
     supervised = timeseries_to_supervised(diff_values, 1)
 
-    lstm_model = fit_lstm(supervised.values, 1, 50, 4)
+    lstm_model, train_acc = fit_lstm(supervised.values, 1, 50, 4)
+
+    print idx, train_acc
 
     save_model(lstm_model, "./models/" + str(idx) + ".kmodel")
 
 
-def make_model_predictions(model, train, all_y, init_val):
+def make_model_predictions(train, all_y, init_val, idx):
 
+    model = load_model("./models/" + str(idx) + ".kmodel")
     # forecast the entire training dataset to build up state for forecasting
     train_reshaped = train[:, 0].reshape(len(train), 1, 1)
 
@@ -111,10 +117,11 @@ def make_model_predictions(model, train, all_y, init_val):
         # store forecast
         predictions.append(yhat)
 
-    return predictions
+    with open("./predictions/" + str(idx) + ".pkl", "wb") as pf:
+        pickle.dump(predictions, pf)
 
 
-def main(retrain=True, bootstrap_method='gp', n_rnns=10):
+def main(retrain=True, bootstrap_method='gp', n_rnns=10, plot_preds=True):
 
     # load dataset
     ex_dataset = pd.read_csv('../../data/mauna-loa-atmospheric-co2.csv',
@@ -153,19 +160,18 @@ def main(retrain=True, bootstrap_method='gp', n_rnns=10):
         else:
             raise Exception("Bootstrap method not implemented!")
 
-        Parallel(n_jobs=10)(delayed(train_model)(pd.DataFrame({'Time': np.ravel(dat[0]),
-                                                               'CO2Concentration': np.ravel(dat[1])}),
-                                                 idx)
-                            for idx, dat in enumerate(bootstrapped_dataset))
+        Parallel(n_jobs=10, verbose=5)(delayed(train_model)(pd.DataFrame({'Time': np.ravel(dat[0]),
+                                                                          'CO2Concentration': np.ravel(dat[1])}),
+                                                            idx)
+                                       for idx, dat in enumerate(bootstrapped_dataset))
 
     """for x, y in bootstrapped_dataset:
         temp_df = pd.DataFrame({'Time': np.ravel(x),
                                 'CO2Concentration': np.ravel(y)})
         rnn_models.append(train_model(temp_df))"""
 
-    rnn_models = []
-    for mod_path in glob.glob("./models/*.kmodel"):
-        rnn_models.append(load_model(mod_path))
+    for pred_path in glob.glob("./predictions/*.pkl"):
+        os.remove(pred_path)
 
     diff_values = ex_dataset['CO2Concentration'].diff()
     # transform data to be supervised learning
@@ -175,9 +181,20 @@ def main(retrain=True, bootstrap_method='gp', n_rnns=10):
     # split data into train and test-sets
     train, test = supervised_values[0:-228], supervised_values[-228:]
 
+    Parallel(n_jobs=20, verbose=5)(delayed(make_model_predictions)(train,
+                                                                   test,
+                                                                   ex_dataset['CO2Concentration'][len(test)+1],
+                                                                   idx)
+                                   for idx in range(n_rnns))
+
     preds = []
-    for mod in rnn_models:
-        preds.append(make_model_predictions(mod, train, test, ex_dataset['CO2Concentration'][len(test)+1]))
+
+    for predf in glob.glob("./predictions/*.pkl"):
+        with open(predf, "rb") as pf:
+            preds.append(pickle.load(pf))
+
+    #for mod in rnn_models:
+    #    preds.append(make_model_predictions(mod, train, test, ex_dataset['CO2Concentration'][len(test)+1]))
 
     # line plot of observed vs predicted
     rnn_means = np.array([])
@@ -185,12 +202,14 @@ def main(retrain=True, bootstrap_method='gp', n_rnns=10):
 
     for k in range(len(preds[0])):
         step_vals = [el[k] for el in preds]
-        rnn_means = np.append(rnn_means, np.mean(step_vals))
+        # rnn_means = np.append(rnn_means, np.mean(step_vals))
+        rnn_means = np.append(rnn_means, stats.trim_mean(step_vals, 0.3))
         rnn_conf = np.append(rnn_conf, np.std(step_vals))
 
     plt.plot(ex_dataset['CO2Concentration'][-228:])
-    for pred in preds:
-        plt.plot(pred, color="black", alpha=.3)
+    if plot_preds:
+        for pred in preds:
+            plt.plot(pred, color="black", alpha=.1)
     plt.plot(rnn_means)
     plt.fill_between(list(range(len(rnn_means))),
                      rnn_means - rnn_conf,
@@ -199,8 +218,23 @@ def main(retrain=True, bootstrap_method='gp', n_rnns=10):
 
     plt.title("Bootstrap Method: " + bootstrap_method + " / Number of RNNs: " + str(n_rnns))
 
-    plt.savefig("./figures/" + bootstrap_method + ".png")
+    plt.savefig("./figures/" + bootstrap_method + "_" + str(n_rnns) +
+                "_samples_" + ["no_preds", "preds"][plot_preds] + ".png")
 
 
 if __name__ == "__main__":
-    main(retrain=True, bootstrap_method='circular_block')
+    main(retrain=False, bootstrap_method='moving_block', n_rnns=10, plot_preds=False)
+
+
+"""
+7 [0.015810276679841896]
+4 [0.003952569169960474]
+2 [0.015810276679841896]
+1 [0.02766798418972332]
+0 [0.011857707509881422]
+3 [0.019762845849802372]
+6 [0.003952569169960474]
+8 [0.011857707509881422]
+5 [0.0079051383399209481]
+9 [0.0]
+"""
