fd2316abadb8e35333e60341d3f580d5e13029aa
Shadi <shadizabad1@gmail.com>
2017-11-24 12:39:32 -0500

    Adding more figures for BNN experiment. Smaller window size works better.

--- BNN_experiments/edward_shadi/bnn_test.py
+++ BNN_experiments/edward_shadi/bnn_test.py
@@ -41,7 +41,7 @@ def neural_network(x, W_0, W_1, W_2, b_0, b_1, b_2):
     return tf.reshape(h, [-1])
 
 
-D = 20   # number of features
+D = 5   # number of features
 
 x_train, y_train = build_co2_dataset(D)
 
@@ -52,7 +52,7 @@ b_0 = Normal(loc=tf.zeros(D), scale=tf.ones(D))
 b_1 = Normal(loc=tf.zeros(D), scale=tf.ones(D))
 b_2 = Normal(loc=tf.zeros(1), scale=tf.ones(1))
 
-x = x_train
+x = x_train[:300]
 y = Normal(loc=neural_network(x, W_0, W_1, W_2, b_0, b_1, b_2),
            scale=0.1 * tf.ones(len(x)))
 
@@ -92,8 +92,8 @@ plt.show()"""
 
 inference = ed.KLqp({W_0: qW_0, b_0: qb_0,
                      W_1: qW_1, b_1: qb_1,
-                     W_2: qW_2, b_2: qb_2}, data={y: y_train})
-inference.run(n_iter=30000)
+                     W_2: qW_2, b_2: qb_2}, data={y: y_train[:300]})
+inference.run(n_iter=15000)
 
 # SECOND VISUALIZATION (posterior)
 
@@ -105,7 +105,7 @@ sd_interval = np.std(outputs, axis=0)
 
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111)
-ax.set_title("Modeling CO2 Dataset with BNNs (Window size: 20)")
+ax.set_title("Modeling CO2 Dataset with BNNs (Window size: %d)" % D)
 ax.plot(list(range(len(outputs[0].T))), y_train, 'ks', alpha=0.5)
 ax.plot(list(range(len(outputs[0].T))), mean_val, 'r', lw=2, alpha=0.5)
 ax.fill_between(list(range(len(outputs[0].T))),
