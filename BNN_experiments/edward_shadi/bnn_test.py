2ed10f98f97bc2e6761427228730d6780760ce3b
Shadi <shadizabad1@gmail.com>
2017-11-24 01:40:04 -0500

    BNN example with Edward is now working! Performance is poor though. Will explore different window sizes / adding more layers to the network.

--- BNN_experiments/edward_shadi/bnn_test.py
+++ BNN_experiments/edward_shadi/bnn_test.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
+from sklearn import preprocessing
 from edward.models import Normal
 
 
@@ -18,6 +18,10 @@ def build_co2_dataset(window_size=20):
                              header=None)
     ex_dataset.columns = ['CO2Concentration', 'Time']
 
+    scaler = preprocessing.StandardScaler()
+    np_scaled = scaler.fit_transform(ex_dataset)
+    ex_dataset = pd.DataFrame(np_scaled, index=ex_dataset.index, columns=ex_dataset.columns)
+
     x = []
     y = []
 
@@ -25,16 +29,8 @@ def build_co2_dataset(window_size=20):
         x.append(ex_dataset['CO2Concentration'][idx:idx + window_size])
         y.append(ex_dataset['CO2Concentration'][idx + window_size])
 
-    x = np.array(x, dtype=np.float32) # .reshape((-1, 1))
+    x = np.array(x, dtype=np.float32)   # .reshape((-1, 1))
     y = np.array(y, dtype=np.float32)
-    return x, y
-
-
-def build_toy_dataset(N=50, noise_std=0.1):
-    x = np.linspace(-3, 3, num=N)
-    y = np.cos(x) + np.random.normal(0, noise_std, size=N)
-    x = x.astype(np.float32).reshape((N, 1))
-    y = y.astype(np.float32)
     return x, y
 
 
@@ -48,10 +44,6 @@ def neural_network(x, W_0, W_1, W_2, b_0, b_1, b_2):
 D = 20   # number of features
 
 x_train, y_train = build_co2_dataset(D)
-
-print x_train
-print len(x_train)
-print x_train.shape
 
 W_0 = Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]))
 W_1 = Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]))
@@ -77,11 +69,7 @@ qb_1 = Normal(loc=tf.Variable(tf.random_normal([D])),
 qb_2 = Normal(loc=tf.Variable(tf.random_normal([1])),
               scale=tf.nn.softplus(tf.Variable(tf.random_normal([1]))))
 
-# Sample functions from variational model to visualize fits.
-rs = np.random.RandomState(0)
-#inputs = np.linspace(-5, 5, num=400, dtype=np.float32)
-#x = tf.expand_dims(inputs, D)
-print x.shape
+
 mus = tf.stack(
     [neural_network(x_train, qW_0.sample(), qW_1.sample(), qW_2.sample(),
                     qb_0.sample(), qb_1.sample(), qb_2.sample())
@@ -89,10 +77,10 @@ mus = tf.stack(
 
 sess = ed.get_session()
 
-print y.eval()
 tf.global_variables_initializer().run()
-outputs = mus.eval()
 
+"""
+outputs = mus.eval()
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111)
 ax.set_title("Iteration: 0")
@@ -100,33 +88,29 @@ ax.plot(x_train, y_train, 'ks', alpha=0.5, label='(x, y)')
 ax.plot(list(range(len(outputs[0].T))), outputs[0].T, 'r', lw=2, alpha=0.5, label='prior draws')
 ax.plot(list(range(len(outputs[0].T))), outputs[1:].T, 'r', lw=2, alpha=0.5)
 #ax.legend()
-plt.show()
+plt.show()"""
 
 inference = ed.KLqp({W_0: qW_0, b_0: qb_0,
                      W_1: qW_1, b_1: qb_1,
                      W_2: qW_2, b_2: qb_2}, data={y: y_train})
-inference.run(n_iter=5000)
-
-print '------------'
-print 'Y eval......'
-print y.eval()
-print '------------'
+inference.run(n_iter=30000)
 
 # SECOND VISUALIZATION (posterior)
 
 outputs = mus.eval()
 
-print outputs[1:].T
-print '------------------------'
-print outputs[0].T
-print '------------------------'
-print y_train
+mean_val = np.mean(outputs, axis=0)
+sd_interval = np.std(outputs, axis=0)
+
 
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111)
-ax.set_title("Iteration: 1000")
-ax.plot(x_train, y_train, 'ks', alpha=0.5, label='(x, y)')
-ax.plot(list(range(len(outputs[0].T))), outputs[0].T, 'r', lw=2, alpha=0.5, label='posterior draws')
-ax.plot(list(range(len(outputs[0].T))), outputs[1:].T, 'r', lw=2, alpha=0.5)
-#ax.legend()
+ax.set_title("Modeling CO2 Dataset with BNNs (Window size: 20)")
+ax.plot(list(range(len(outputs[0].T))), y_train, 'ks', alpha=0.5)
+ax.plot(list(range(len(outputs[0].T))), mean_val, 'r', lw=2, alpha=0.5)
+ax.fill_between(list(range(len(outputs[0].T))),
+                mean_val - 2 * sd_interval,
+                mean_val + 2 * sd_interval,
+                color="red", alpha=0.2)
+ax.plot()
 plt.show()
