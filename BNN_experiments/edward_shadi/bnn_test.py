05b5dc758f1ada0a53c913fc59ef7cf40e9dfd04
Shadi <shadizabad1@gmail.com>
2017-11-25 18:29:57 -0500

    Refactoring BNN code to allow user to specify any number of hidden layers.

--- BNN_experiments/edward_shadi/bnn_test.py
+++ BNN_experiments/edward_shadi/bnn_test.py
@@ -1,4 +1,7 @@
 """
+Author: Shadi Zabad
+Date: November 2017
+-------------------
 Code below modifies implementation given in:
 http://nbviewer.jupyter.org/github/blei-lab/edward/blob/master/notebooks/getting_started.ipynb
 """
@@ -14,6 +17,11 @@ from edward.models import Normal
 
 
 def build_co2_dataset(window_size=20):
+    """
+    Read the CO2 dataset and break it into sliding windows of <window_size>.
+    The windows will serve as inputs to the BNN and the output will be the
+    value of the next element in the sequence.
+    """
     ex_dataset = pd.read_csv('../../data/mauna-loa-atmospheric-co2.csv',
                              header=None)
     ex_dataset.columns = ['CO2Concentration', 'Time']
@@ -29,84 +37,102 @@ def build_co2_dataset(window_size=20):
         x.append(ex_dataset['CO2Concentration'][idx:idx + window_size])
         y.append(ex_dataset['CO2Concentration'][idx + window_size])
 
-    x = np.array(x, dtype=np.float32)   # .reshape((-1, 1))
+    x = np.array(x, dtype=np.float32)
     y = np.array(y, dtype=np.float32)
+
     return x, y
 
 
-def neural_network(x, W_0, W_1, W_2, b_0, b_1, b_2):
-    h = tf.tanh(tf.matmul(x, W_0) + b_0)
-    h = tf.tanh(tf.matmul(h, W_1) + b_1)
-    h = tf.matmul(h, W_2) + b_2
+def neural_network(x, weights_biases):
+
+    h = tf.tanh(tf.matmul(x, weights_biases[0][0]) + weights_biases[0][1])
+
+    for lyr in weights_biases[1:-1]:
+        h = tf.tanh(tf.matmul(h, lyr[0]) + lyr[1])
+
+    h = tf.matmul(h, weights_biases[-1][0]) + weights_biases[-1][1]
+
     return tf.reshape(h, [-1])
 
 
-D = 5   # number of features
+def build_networks(n_inputs=5, n_samples=10, n_layers=None):
 
-x_train, y_train = build_co2_dataset(D)
+    x_, y_ = build_co2_dataset(n_inputs)
 
-W_0 = Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]))
-W_1 = Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]))
-W_2 = Normal(loc=tf.zeros([D, 1]), scale=tf.ones([D, 1]))
-b_0 = Normal(loc=tf.zeros(D), scale=tf.ones(D))
-b_1 = Normal(loc=tf.zeros(D), scale=tf.ones(D))
-b_2 = Normal(loc=tf.zeros(1), scale=tf.ones(1))
+    if n_layers is None:
+        alpha = 10.0
+        n_layers = int(float(len(x_)) / (alpha * (n_inputs + 1))) + 1
 
-x = x_train[:300]
-y = Normal(loc=neural_network(x, W_0, W_1, W_2, b_0, b_1, b_2),
-           scale=0.1 * tf.ones(len(x)))
+    print "Creating neural networks with", str(n_layers + 1), "layers"
 
-qW_0 = Normal(loc=tf.Variable(tf.random_normal([D, D])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([D, D]))))
-qW_1 = Normal(loc=tf.Variable(tf.random_normal([D, D])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([D, D]))))
-qW_2 = Normal(loc=tf.Variable(tf.random_normal([D, 1])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([D, 1]))))
-qb_0 = Normal(loc=tf.Variable(tf.random_normal([D])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([D]))))
-qb_1 = Normal(loc=tf.Variable(tf.random_normal([D])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([D]))))
-qb_2 = Normal(loc=tf.Variable(tf.random_normal([1])),
-              scale=tf.nn.softplus(tf.Variable(tf.random_normal([1]))))
+    weights_biases = []
+    q_weights_biases = []
+    w_b_dict = dict()
 
+    for nl in range(n_layers):
+        w_i = Normal(loc=tf.zeros([n_inputs, n_inputs]),
+                     scale=tf.ones([n_inputs, n_inputs]))
+        b_i = Normal(loc=tf.zeros(n_inputs),
+                     scale=tf.ones(n_inputs))
+        qw_i = Normal(loc=tf.Variable(tf.random_normal([n_inputs, n_inputs])),
+                      scale=tf.nn.softplus(tf.Variable(tf.random_normal([n_inputs, n_inputs]))))
+        qb_i = Normal(loc=tf.Variable(tf.random_normal([n_inputs])),
+                      scale=tf.nn.softplus(tf.Variable(tf.random_normal([n_inputs]))))
 
-mus = tf.stack(
-    [neural_network(x_train, qW_0.sample(), qW_1.sample(), qW_2.sample(),
-                    qb_0.sample(), qb_1.sample(), qb_2.sample())
-     for _ in range(10)])
+        weights_biases.append((w_i, b_i))
+        q_weights_biases.append((qw_i, qb_i))
+
+        w_b_dict[w_i] = qw_i
+        w_b_dict[b_i] = qb_i
+
+    w_i = Normal(loc=tf.zeros([n_inputs, 1]),
+                 scale=tf.ones([n_inputs, 1]))
+    b_i = Normal(loc=tf.zeros(1),
+                 scale=tf.ones(1))
+    qw_i = Normal(loc=tf.Variable(tf.random_normal([n_inputs, 1])),
+                  scale=tf.nn.softplus(tf.Variable(tf.random_normal([n_inputs, 1]))))
+    qb_i = Normal(loc=tf.Variable(tf.random_normal([1])),
+                  scale=tf.nn.softplus(tf.Variable(tf.random_normal([1]))))
+
+    weights_biases.append((w_i, b_i))
+    q_weights_biases.append((qw_i, qb_i))
+    w_b_dict[w_i] = qw_i
+    w_b_dict[b_i] = qb_i
+
+    y = Normal(loc=neural_network(x_, weights_biases),
+               scale=0.1 * tf.ones(len(x_)))
+
+    mus = tf.stack(
+        [neural_network(x_, [(w.sample(), b.sample()) for w, b in q_weights_biases])
+         for _ in range(n_samples)])
+
+    return mus, {y: y_}, w_b_dict, y_
+
 
 sess = ed.get_session()
-
 tf.global_variables_initializer().run()
 
-"""
-outputs = mus.eval()
-fig = plt.figure(figsize=(10, 6))
-ax = fig.add_subplot(111)
-ax.set_title("Iteration: 0")
-ax.plot(x_train, y_train, 'ks', alpha=0.5, label='(x, y)')
-ax.plot(list(range(len(outputs[0].T))), outputs[0].T, 'r', lw=2, alpha=0.5, label='prior draws')
-ax.plot(list(range(len(outputs[0].T))), outputs[1:].T, 'r', lw=2, alpha=0.5)
-#ax.legend()
-plt.show()"""
+n_iterations = 5000  # Number of iterations
+n_features = 2  # Number of inputs to the neural network
 
-inference = ed.KLqp({W_0: qW_0, b_0: qb_0,
-                     W_1: qW_1, b_1: qb_1,
-                     W_2: qW_2, b_2: qb_2}, data={y: y_train[:300]})
-inference.run(n_iter=15000)
+# Number of inputs is set to 5, we sample 10 times
+mus, y_dict, w_dict, y_vals = build_networks(n_inputs=n_features,
+                                             n_samples=10,
+                                             n_layers=4)
 
-# SECOND VISUALIZATION (posterior)
+
+inference = ed.KLqp(w_dict, data=y_dict)
+inference.run(n_iter=n_iterations)
 
 outputs = mus.eval()
 
 mean_val = np.mean(outputs, axis=0)
 sd_interval = np.std(outputs, axis=0)
 
-
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111)
-ax.set_title("Modeling CO2 Dataset with BNNs (Window size: %d)" % D)
-ax.plot(list(range(len(outputs[0].T))), y_train, 'ks', alpha=0.5)
+ax.set_title("Modeling CO2 Dataset with BNNs (Window size: %d)" % n_features)
+ax.plot(list(range(len(outputs[0].T))), y_vals, 'ks', alpha=0.5)
 ax.plot(list(range(len(outputs[0].T))), mean_val, 'r', lw=2, alpha=0.5)
 ax.fill_between(list(range(len(outputs[0].T))),
                 mean_val - 2 * sd_interval,
